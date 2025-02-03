48ceca5f56b849c43e81fdf05efecee89048dd19
Amarnani506 <158752545+Amarnani506@users.noreply.github.com>
2025-02-03 17:24:11 +0530

    Delete website directory

--- website/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const buttons = document.querySelectorAll("button");
-
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert("Added to cart!");
-        });
-    });
-});
